73f8e2378ef520f739e7a8132cfffbb62d508ea5
Rufael Mekuria <rufael.mekuria@cwi.nl>
2015-03-12 17:15:47 +0100

    removed vs stuff

--- cloud_codec_v2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/mpeg/git_jpeg/pcl/cloud_codec_v2
-# Build directory: C:/mpeg/git_jpeg/pcl/cloud_codec_v2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- quality/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/mpeg/git_jpeg/pcl/quality
-# Build directory: C:/mpeg/git_jpeg/pcl/quality
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
